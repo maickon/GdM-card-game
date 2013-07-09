842385d61f5077e53d51a65ade97928c8a6e0e8b
Maickon <maickonmaickon@hotmail.com>
2013-07-09 20:33:49 -0300

    GdM card game

---
+++ classes/artefatos.php
@@ -0,0 +1,11 @@
+<?php
+$carta1 = new ArtefatoCarta('Espada Berserker','GdM','Efeito: Só pode ser equpada em guerreiros. A criatura equipada ganha a habilidade berserker. E ganha +1 no ATK.','Mag 1','For 4');
+$carta2 = new ArtefatoCarta('Espada de  Boors','GdM','Efeito Restrito para guerreiros. Aumenta +2 no ATK do guerreiro equipado.','Mag 0','For 3');
+$carta3 = new ArtefatoCarta('Elmo Medieval','GdM','Efeito Só guerreiros podem ser equipados com este elmo. Aumenta +1 na DEF da criatura equipada.','Mag 0','For 2');
+
+$cartas = array($carta1,
+				$carta2,
+				$carta3,				
+);
+
+?>
---
+++ efeitos.php
@@ -0,0 +1,280 @@
+<h1>Livros de Efeitos</h1>
+<p>Os efeitos descrevem as habilidades que as criaturas possuem, podem ser habilidades mágias ou de combates.
+</p>
+<p>Abaixo segue a lista de todoas os efeitos deste Card Game.</p>
+<p class="efeito"><strong>Abalar</strong><br />
+quando essa criatura der um ataque sucedido(que acerte os Pvs) 
+você joga 1d6 e se der um numero pár,a criatura atacada fica 2 rodas com RES e DEF reduzidas a 0.</p>
+
+<p class="efeito"><strong>Regeneração</strong><br />
+Você recupera 1 ponto de vida dessa criatura ao final de um combate.
+</p>
+
+<p class="efeito"><strong>Comando</strong><br />
+Você ganha +1 no ATK enquanto tiver uma criatura da Classe Lord-Guerreiro ou Guerreiro-Lord em campo,
+esse bonûs se acumula caso tenha mais de uma criatura do tipo Lord-Guerreiro ou Guerreiro-Lord em campo.
+</p>
+
+<p class="efeito"><strong>Fúria</strong><br />
+- A cada 2 Lv a criatura recebe +1 no ATK até o final do turno e perde 1 na DEF permanente.
+Essa Habilidade pode ser usada uma vez por turno,a qualquer momento.
+</p>
+
+<p class="efeito"><strong>Ataque Poderoso</strong><br />
+Durante o combate a criatura pode reduzir sua DEF limitada pelo nivel e colocar no ATK,
+(Exemplo: personagem Lv3 pode tirar até 3 da DEF e colocar no ATK),(no final do turno a defesa volta ao normal).
+</p>
+
+<p class="efeito"><strong>Ataque poderoso aprimorado</strong><br />
+Quase igual ao anterior, porém para cada pt retirado da defesa a criatura coloca +2 no ataque e a redução da defesa não é limitada pelo nivel.</p>
+
+<p class="efeito"><strong>Disparo Fantasma (Somente Arqueiro)</strong><br />
+Esta criatura quando ataca tem a habilidade de causar 1 de dano a criatura em combate idependente de acerta-la ou não.</p>
+
+<p class="efeito"><strong>Honra</strong><br />
+Para cada 2 Lv da criatura,ele reduzirá 1 ponto de dano durante o combate,(só criaturas de Lv2 ou maior possuem essa habilidade).</p>
+
+<p class="efeito"><strong>Ataque Duplo</strong><br />
+A criatura é capaz de realizar 2 ataques durante o combate. Obs: se a criatura com ataque duplo morrer com o ataque do seu inimigo, ele não realiza o seu segundo ataque. Pois o primeiro ataque é simultâneo ambas as criaturas atacam uma a outra juntas, 
+porém uma criatura com ataque duplo que sobreviva ao primeiro ataque, pode executar o seu segundo ataque ao seu inimigo.</p>
+
+<p class="efeito"><strong>Contra ataque</strong><br />
+Quando uma criatura for alvo de um ataque,ela realiza o contra ataque antes de sofrer qualquer dano.</p>
+
+<p class="efeito"><strong>Sacrificio</strong><br />
+Durante o seu turno você pode sacrificar esta criatura e causar metade do seu Lv em dano a uma criatura alvo,(O dano é arredondado para baixo. 
+Exemplo: criatura Lv 3 da 2 de dano).</p>
+
+<p class="efeito"><strong>Sacrificio Poderoso</strong><br />
+Durante o combate a criatura pode sacrificar seus Pvs e colocalos no seu ATK,no final do turno o ATK volta ao normal,
+(a criatura nunca poderá sacrificar mais vida do que o seu Lv. Exemplo: criatura de Lv 3 só poderá sacrificar até 3 pontos de Pv,
+(sua vida nunca poderá ser reduzida a 0 para utilizar essa habilidade).</p>
+
+<p class="efeito"><strong>Honra Divina (Somente Paladinos e Anjos)</strong><br />
+Quando essa criatura esta em campo ela não coexiste com nenhuma criatura com tendencia maligna,
+e caso essa criatura esteja no campo e uma criatura maligna também no seu campo a mais fraca em Lv das 2 será destruida,
+(caso tenha mais de 1 criatura maligna ou criatura com essa habilidade,
+será somado os Lvs das criaturas,e o lado que tiver menos Lv será destruido).</p>
+
+<p class="efeito"><strong>Ira da Guerra</strong><br />
+Quando essa criatura ataca uma criatura corpo a corpo ela ganha mais 2 de ATK até o final do turno,(isso não se aplica a contra ataques).</p>
+
+<p class="efeito"><strong>Convicção</strong><br />
+Você ganha inspiração e aumenta sua DEF em +2,você só pode ativar essa habilidade 1 vez a cada 2 turnos),
+(no final do turno que você usou convicção,sua DEF volta ao normal).</p>
+
+<p class="efeito"><strong>Estraçalhar</strong><br />
+Qualquer criatura que bloquear o ataque dessa criatura, perde 2 na DEF ate o final do turno.</p>
+
+<p class="efeito"><strong>Foco em arma</strong><br />
+A criatura ganha +1 no ATK quando entra na fase de batalha</p>
+
+<p class="efeito"><strong>Foco em arma aprimorado</strong><br />
+A criatura ganha +2 no ATK quando entra na fase de batalha</p>
+
+<p class="efeito"><strong>Iniciativa</strong><br />
+A iniciativa garante que caso o seu ataque seje suficuente para matar a criatura inimiga, 
+ela morrerá sem ter a oportunidade de te atacar. Obs: normalmente o fluxo de combate ocorre com as duas criaturas atacando simultaneamente,
+ no caso da iniciativa você ataca primeiro.</p>
+ 
+<p class="efeito"><strong>Primeiros socorros</strong><br />
+A criatura com primeiros socorros possui algum recurso que lher permite dar assistencia a um parceiro debilitado. 
+Seus recurssos podem vir de diversas coisas como: conhecimento em tratar ferimentos, alguma poção que carrega,
+algum remedio e etc. Uma criatura que esteja prestando seus primeiros socorros  
+deve permenecer com a face da carta virada para baixo sobe a criatura a ser auxiliada. 
+A cada rodada que a criatura permanece assim ela estará curando a criatura necessitada.  
+O nivel da criatura define o quanto poderoso é o seu socorro. 
+<br />
+Lv 1 – 3		+1Pv <br />		
+Lv 4 – 6		+2Pv <br />
+Lv 7 – 9		+3Pv <br />		
+Lv 10 – 12	    +4Pv <br />
+
+</p>
+
+<p class="efeito"><strong>Presença celestial - (apenas criaturas celestiais de nivel 12 ou maior possuem esta habilidade)</strong><br />
+Quando esta criatura entra em campo, destrua todas as outras criaturas de tendencia maligna com 4 niveis menor do que ela.</p>
+
+<p class="efeito"><strong>Música de bardo (somete bardos)</strong><br />
+Uma música encantadora paraliza uma criatura alvo. Tornando- a imcapaz de executar qualquer coisa.
+ O número de rodadas em que a vítima fica paralizada dependerá do nivel do bardo.
+<br />
+Lv1 – 1 rodada custo 3 <br />
+Lv2 – 2 rodada custo 4 <br />
+Lv3 – 3 rodada custo 5 <br />
+Lv4 – 4 rodada custo 6 <br />
+Lv5 – 5 rodada custo 7 <br />
+Lv6 – 6 rodada custo 8 <br />
+Lv7 – 7 rodada custo 9 <br />
+Lv8 – 8 rodada custo 10 <br />
+Lv9 – 9 rodada custo 11 <br />
+Lv10 – 10 rodada custo 12 <br />
+Lv11 – 11 rodada custo 13 <br />
+Lv12 – 12 rodada custo 14 <br />
+</p>
+
+<p class="efeito"><strong>Bushido</strong><br />
+Essa criatura ganha +1 no ATK para o contra ataque,(No fim do turno o ATK volta ao normal).</p>
+
+<p class="efeito"><strong>Provocar</strong><br />
+Essa criatura pode atacar qualquer criatura de seu oponente.(Independente se ele defender ou não,você escolhe uma criatura alvo e ela é obrigada a defender).</p>
+
+<p class="efeito"><strong>Prontidão</strong><br />
+Essa criatura pode defender quantas crituras quiser e ganha +2 na DEF.</p>
+
+<p class="efeito"><strong>Golpe alado (Anjo)- </strong><br />
+Um guerreiro desce dos ceus,para desferir um golpe fatal em todos seus inimigos jurados. Aumenta +2 no ATK caso equipado com um artefato.(Esse bônus não é acumulativo).</p>
+
+<p class="efeito"><strong>Invunerável</strong><br />
+Essa criatura não é afetada por nenhum estatos negativo,(exemplo: congelamento,veneno etc...).</p>
+
+<p class="efeito"><strong>Agilidade</strong><br />
+A criatura que possui essa habilidade pode atacar no turno que é baixada.</p>
+
+<p class="efeito"><strong>Berserker</strong><br />
+A criatura se transforma em uma besta sedenta em sangue,que só sente prazer em destruir tudo ao seu redor. 
+( Quando sofre dano essa habilidade é ativada),(concede a criatura +2 no ATK e sua tendencia passa a ser (C/M).</p>
+
+<p class="efeito"><strong>Perseguir</strong><br />
+Essa criatura é obrigada a atacar todas as criaturas inimigas no seu turno de ataque. 
+A cada combate que ela sobreviva ela prossegue para o próximo ataque contra autra criatura até morrer ou 
+quando atingir os pontos de vida do jogador.
+(Quando ela for atacar uma nova criatura ela ganha +1 de DEF até o fim do turno)</p>
+
+<p class="efeito"><strong>Ninjutsu de Fogo</strong><br />
+Cria uma aura de fogo em torno de sí,e aumenta o dano do ataque dessa criatura em +1.
+(Quando você inflinge dano em uma criatura você da 1 ponto de dano adicional).</p>
+
+<p class="efeito"><strong>Taijutsu</strong><br />
+Aumenta o ATK dessa criatura em +2 durante o combate.(Esse efeito só é ativado quando você ataca uma criatura),
+(não é ativado durante o contra ataque).</p>
+
+<p class="efeito"><strong>Terror</strong><br />
+Você aterreoriza seus inimigos com sua presença. 
+A criatura que possui essa habilidade não pode ser defendida por nenhuma criatura no turno que usa essa habilidade. 
+(Você só pode usar essa habilidade novamente a cada 1d4 de turnos).
+</p>
+
+<p class="efeito"><strong>Disparo da Misericórdia(Arqueiro) </strong><br />
+Se essa criatura transpassar a defesa da criatura inimiga,você pode dar um ataque extra.
+</p>
+
+<p class="efeito"><strong>Disparo Duplo(Arqueiro)</strong><br />
+Da 2 ataques consecultivos na criatura alvo.Isso se aplica a contra ataques.
+</p>
+
+<p class="efeito"><strong>Battoujutsu(Samurai)</strong><br />
+Você ataca os seus inimigos com uma velocidade sobre-humana.Você da ataques até matar a criatura inimiga,
+sendo que a cada ataque que você der a criatura atacada fará um contra ataque.
+</p>
+
+<p class="efeito"><strong>Aura Samurai(Samurai)</strong><br />
+Quando equipado com algum artefato que tenha no nome "Katana",ganha +1 de ATK e +1 de DEF.</p>
+
+<p class="efeito"><strong>Força de Vontade(Samurai)</strong><br />
+Você é imune a magias usadas por criaturas de Lv8 para baixo.</p>
+
+<p class="efeito"><strong>Reflexo Sobrenatural</strong><br />
+Seu corpo começa a reagir sozinho a os ataques de seus inimigos.
+Toda vez que você for atacado você pode revidar dando 1d6 de dano na criatura que te atacou).
+</p>
+
+<p class="efeito"><strong>Ninjutsu das Sombras</strong><br />
+A criatura que você atacar não poderá te contra atacar,
+nem usar efeito ou magia alguma contra você.Você pode usar essa habilidade a cada 1d4 de turnos.</p>
+
+<p class="efeito"><strong>Vento forte</strong><br />
+Um vento muito forte sai de sua espada arremesando seus inimigos para longe e os fazendo perde a vontade de lutar.
+Quando um ataque seu atinge o Pv de uma criatura ela é impossibilitada de atacar no proximo turno.(Isso acontece no contra ataque também,
+caso no contra ataque você atinja o Pv de uma criatura,no proximo turno essa criatura não poderá atacar).</p>
+
+<p class="efeito"><strong>Dai Lord(Samurai)</strong><br />
+Enquanto essa criatura estiver no campo,ela aumenta o ATK de todos as criaturas da classe Samurai em +1.(Isso não se aplica a ela mesmo).</p>
+
+<p class="efeito"><strong>Garra Negra</strong><br />
+Quando você da um ataque corpo a corpo,você da 1d6 adicional.</p>
+
+<p class="efeito"><strong>Ninjutsu de Terra</strong><br />
+Você se esconde em baixo da terra evitando qualquer ataque de seus inimigos.
+(No turno que você usar essa habilidade você não recebe dano nenhum de ataques inimigos,
+mesmo que você defenda os ataques),(você só pode usar essa habilidade novamente a cada 1d4 de turnos).</p>
+
+<p class="efeito"><strong>Táticas de Combate</strong><br />
+Você aprende uma habilidade a sua escolha de uma criatura alvo no seu campo.(Você só pode fazer isso uma vez).</p>
+
+<p class="efeito"><strong>Katana Sobrenatural(Samurai) Lv1 e Lv2</strong><br />
+Quando essa criatura entra em campo você busca em seu deck um artefato que tenha no nome "katana"
+ e equipa nessa criatura. Essa habilidade possui 2 Lv,no segundo Lv você busca e equipa 2 katanas. 
+</p>
+
+<p class="efeito"><strong>Lâmina do Dragão(Samurai)</strong><br />
+Quando você esta equipado com um artefato que possui no nome "Katana",durante o combate você da 1d8 junto com cada um de seus ataques. 
+"Seus golpes são tão velozes que cria um dragão feito com a luz de sua Lâmina para dilacerar seus oponentes".
+</p>
+
+<p class="efeito"><strong>Washizaki(Samurai)</strong><br />
+Sua honra é tão grande,que não suporta receber um único golpe de um inimigo. 
+Quando essa criatura recebe dano de combate no Pv,ela se auto sacrifica,e vai para o cemitério.
+</p>
+
+<p class="efeito"><strong>Corte Flamejante</strong><br />
+Você da 1d4 de dano de fogo em todos os ataques que você da na criatura inimiga.
+</p>
+
+<p class="efeito"><strong>Arremeçar lança</strong><br />
+Você joga uma lança que perfura seus inimigos.(Você lança um artefato em uma criatura 
+alvo dando 2d8 de dano nela,o artefato e destruido quando você fizer isso),
+(Isso não conta como uma fase de combate,então você ainda poderá atacar seu oponente),
+(essa habilidade pode ser usada durante o combate para aumentar o dano de seu ataque).
+</p>
+
+<p class="efeito"><strong>Ninten Ichi Ryu</strong><br />
+Você da você da um ataque adicional se estiver equipado com um artefato que tenha no nome "Katana".
+</p>
+
+<p class="efeito"><strong>Chamado de Guerra</strong><br />
+Quando essa criatura entra em jogo você pode colocar uma criatura que conpartilhe 1 classe com ela do seu deck em campo,
+você joga a criatura do seu deck como se fosse da sua mão,pagando o custo de Pv para adicionar ao multiplicador.
+</p>
+
+<p class="efeito"><strong>Corte Violento</strong><br />
+Quando você  da um ataque que atinge o Pv de uma criatura você joga 1d8 e o resultado 
+se torna o número de turnos que a criatura que você atacou vai demorar para ser destruida.
+(Exemplo: dei dano na criatura e joguei o dado,e deu 4 no dado,em 4 turnos a criatura é 
+automaticamente destruida e enviada para o cemitério).(Essa habilidade só é ativada novamente depois de 1d4 de turnos).
+</p>
+
+<p class="efeito"><strong>Chuva das Mil Flechas(Arqueiro)</strong><br />
+Você da um ATK em cada criatura de seus inimigos.As criaturas inimigas não podem contra atacar.Isso conta como 1 ataque seu.
+</p>
+
+
+<p class="efeito"><strong>Criação de Armas(Ferreiro)</strong><br />
+Um ferreiro que têm essa habilidade pode colocar qualquer artefato em campo com um custo de FOR reduzido em 1.
+</p>
+
+<p class="efeito"><strong>Aumento da Fé(Paladino)</strong><br />
+Sua fé se torna cada vez mais forte,e os Deuses o abençoam com grande poder para combater o mau.Quando entra em combate com criaturas Malignas você ganha +2 no ATK e +2 na DEF.No fim do combate seu ATK e DEF voltam ao normal.
+</p>
+
+<p class="efeito"><strong>Destruir a Fé(Algoz)</strong><br />
+Seu corpo emana um forte odor de caos e destruição,as criaturas ao seu redor tremem de medo e têm sua fé abalada pela sua presença...A fé das criaturas é destruida aos poucos até que os Deuses as abandonem. Quando você ataca uma criatura com tendência boa você ganha +2 no ATK e diminui a DEF da criatura atacada em -1 permanente,a cada ataque que você der na criatura ela ganha um marcador -1 na DEF.Seu ATK volta ao normal no final do combate.
+</p>
+
+<p class="efeito"><strong>Ameçar</strong><br />
+Sua presença é ameaçadora para todas as criaturas vivas...Seu instinto te leva a destruir a todos ao seu redor.Quando uma criatura entrar em jogo(no campo de seus oponentes) você pode dar um ataque nela no turno de seu oponente,esse ataque em vez de rolar 1d6 você rola 1d10,a criatura não pode contra atacar.Toda vez que você usa essa habilidade você perde -1 de DEF permanente.
+</p>
+
+<p class="efeito"><strong>Violento</strong><br />
+Você fica cada vez mais agressivo,ao ponto que não consegue se controlar e vira um ser de pura violência.Você ganha +1 de ATK no inicio de cada turno seu. Essa habilidade possui 4 Lvs,a cada Lv você pode adicionar um limite maior.(Exemplo: Uma criatura com Violênto Lv2 pode ganhar no maximo +2 no ATK e depois para o ganho).A cada Lv a criatura ganha um limite de +1 na habilidade Violênto.
+</p>
+
+<p class="efeito"><strong>Falange</strong><br />
+Seus compatriotas se juntam a você para forma uma falange impenetravel.Você aumenta sua DEF e RES em +1 para cada criatura da mesma raça que você no campo.
+</p>
+
+<p class="efeito"><strong></strong><br />
+</p>
+
+
+
